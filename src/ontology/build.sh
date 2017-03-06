81f6b7373d05806d980764abf9e4c2ba752d868d
marijane white <whimar@ohsu.edu>
2017-03-05 23:24:04 -0800

    Add ohsu-education.ttl and ohsu-research.ttl to merge command

--- src/ontology/build.sh
+++ src/ontology/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-robot merge -i module/ohsu-general.ttl -i module/ohsu-healthcare.ttl -i mireot/schema.org-mireot.ttl -i mireot/health-lifesci-mireot.ttl -i bridge/schema.org-bridge.ttl annotate --annotation-file build-annotations.ttl -o ohsu-schema.ttl
+robot merge -i module/ohsu-general.ttl -i module/ohsu-healthcare.ttl -i module/ohsu-education.ttl -i module/ohsu-research.ttl -i mireot/schema.org-mireot.ttl -i mireot/health-lifesci-mireot.ttl -i bridge/schema.org-bridge.ttl annotate --annotation-file build-annotations.ttl -o ohsu-schema.ttl
