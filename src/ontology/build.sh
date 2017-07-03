ba88e6926e5d075ee2cf8b56920d936d3f1672ba
marijane white <whimar@ohsu.edu>
2017-07-02 17:08:08 -0700

    remove environment-specific copy command

--- src/ontology/build.sh
+++ src/ontology/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 robot merge -i module/ohsu-general.ttl -i module/ohsu-healthcare.ttl -i module/ohsu-education.ttl -i module/ohsu-research.ttl -i mireot/schema.org-mireot.ttl -i mireot/health-lifesci-mireot.ttl -i bridge/schema.org-bridge.ttl annotate --annotation-file build-annotations.ttl -o ohsu-schema.ttl
-cp -f ohsu-schema.ttl ~/git/ohsu-library/ohsu-data/karma/preloaded-ontologies/
+
+# cp -f ohsu-schema.ttl ~/git/ohsu-library/ohsu-data/karma/preloaded-ontologies/
